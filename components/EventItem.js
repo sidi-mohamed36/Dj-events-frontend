bf852aaba7401ebb8fe4241511eb0f5c7ae9737f
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-11 03:35:29 +0100

    Eventitem

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '@/styles/EventItem.module.css'
 export default function EventItem({evt}) {
