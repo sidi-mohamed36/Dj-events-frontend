a693a681877907735b542cdd10e16ebe1b0f9bfa
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-15 11:43:22 +0100

    third commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function EventsPage({events}) {
     <div >
        <Layout>
        <h1>Events</h1>
-         {events.length === 0 && <h3>No events to show</h3>
+        {events.length === 0 && <h3>No events to show</h3>
          }
        {events.map((evt) => (
          <EventItem key={evt.id} evt={evt}/>)
