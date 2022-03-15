ac907c5103e1adcfdac81423e1e3327976c4227a
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-15 12:08:24 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ async  headers() {
   return [
     {
       // matching all API routes
-      source: "/api/:path*",
+     
       headers: [
         { key: "Access-Control-Allow-Credentials", value: "true" },
         { key: "Access-Control-Allow-Origin", value: "*" },
