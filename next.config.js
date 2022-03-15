d3fe162c2797cfb2ac933cba5e1303135a3e9b05
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-15 12:36:54 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-    source: "/api/:path*",
+    source: "/api/*",
     headers: [
       { key: "Access-Control-Allow-Credentials", value: "true" },
       { key: "Access-Control-Allow-Origin", value: "*" },
