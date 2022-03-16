dc45ceb9aefb69e277e67dd8ae95b5fb5da6a0d9
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-16 01:28:02 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -3,12 +3,19 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  src: "/api/(.*)",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    "Access-Control-Allow-Credentials": "true"
-  },
-  continue: true
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
