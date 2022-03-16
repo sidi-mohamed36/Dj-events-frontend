b607c6976e158dbbc7d12157fafa8b664f9c207d
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-16 02:03:21 +0100

    commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -32,7 +32,7 @@ export default async (req, res) => {
         })
       )
 
-      res.status(200).json({ user: data.user })
+      res.status(200).json({ user: data.user }).send("ok")
     } else {
       res
         .status(data.statusCode)
