c813a0a97a5f4b4fa0f5b2d35833460588e0b5ed
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-16 02:51:46 +0100

    commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -16,15 +16,7 @@ export default async (req, res) => {
         password,
       }),
     })
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method == "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
+   
     const data = await strapiRes.json()
 
     if (strapiRes.ok) {
@@ -39,7 +31,12 @@ export default async (req, res) => {
           path: '/',
         })
       )
-
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
       res.status(200).json({ user: data.user })
     } else {
       res
