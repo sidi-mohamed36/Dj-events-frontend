c2cb5743e079044f2fca11b61be4f2c2d79cbec8
sidi mohamed <sidimed.khercheve@gmail.com>
2022-03-16 02:48:51 +0100

    commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -21,11 +21,10 @@ export default async (req, res) => {
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+    if (req.method == "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
     const data = await strapiRes.json()
 
     if (strapiRes.ok) {
